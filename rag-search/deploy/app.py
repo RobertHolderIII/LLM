6aa6ab88bf0b868cdc428ebdac9575223bc82915
Robert Holder <holder1@umbc.edu>
2025-05-17 21:40:35 -0400

    split vector generation and retrieval

--- rag-search/deploy/app.py
+++ rag-search/deploy/app.py
@@ -41,12 +41,17 @@ def get_text(url):
     text = BeautifulSoup(html, features="html.parser").get_text().strip()
     return text
 
-def create_vector_store(urls):
-    documents = [get_text(url) for url in urls]
+chroma_client = chromadb.Client()
+def create_vector_store(search_terms):
+    urls = web_search(search_terms)
+    documents = []
+    for i,url in enumerate(urls):
+        documents.append(get_text(url))
+        yield f'downloading {i+1}/{len(urls)}: {url}'
     ids = [f'id{i}' for i in range(len(documents))]
     metadatas = [{'source-url': url} for url in urls]
+    
 
-    chroma_client = chromadb.Client()
     collection_name = "webpages"
 
     # get rid of old collection
@@ -63,8 +68,8 @@ def create_vector_store(urls):
     # TODO need to handle the case where search_res has a URL that we did not use
     # `add` uses Chroma's default sentence embedding model
     collection.add(documents=documents, ids=ids, metadatas=metadatas)
-    print(f'collection has {collection.count()} documents')
-    return collection
+    yield f'Document store recreated with {collection.count()} documents'
+    
 
 def retrieve_documents(collection):
     retrieval = collection.query(
@@ -74,14 +79,17 @@ def retrieve_documents(collection):
     )
     return retrieval
 
-def generate_summaries(user_question, history):
-    search_res = web_search()
-    collection = create_vector_store(search_res)
+def generate_summaries():
+    collection = chroma_client.get_collection("webpages")
     retrieval = retrieve_documents(collection)
     relevant_docs = retrieval['documents'][0]
-    print(f'summarizing {len(relevant_docs)} documents')
-    summaries = [summarize(client,doc[:5000]) for doc in relevant_docs]
 
+    summaries = []
+    for i, doc in enumerate(relevant_docs):
+        yield f'summarizing {i+1}/{len(relevant_docs)} documents','In progress...'
+        summaries.append(summarize(client,doc[:5000]))
+
+    
     # format summaries
     out = ''
     metadatas = retrieval['metadatas'][0]
@@ -89,7 +97,7 @@ def generate_summaries(user_question, history):
     for idx, (url, summary) in enumerate(zip(urls, summaries)):
         out += (f'{idx+1}: {url}\n---------------------\n{summary}\n\n')
 
-    return out
+    yield f'summarized {len(relevant_docs)} documents', out
 
 with gr.Blocks() as demo:
     gr.Markdown(
@@ -99,7 +107,23 @@ with gr.Blocks() as demo:
         Instructions go here
         """
         )
-    gr.ChatInterface(fn=generate_summaries, type="messages")
+    search_terms = gr.Dropdown(label='search query', show_label=True, allow_custom_value=True,
+                               choices=["US tariff news, analysis, and predictions",
+                                        "world reaction to US tariff policy"
+                                        ]
+
+                               )
+    update_btn = gr.Button('Update documents')
+
+    status = gr.Textbox(show_label=False)
+    
+    sum_btn = gr.Button('Generate summaries')
+    sum_status = gr.Textbox(show_label=False)
+    out = gr.TextArea(label='Summarizations', show_label=True)
+
+    update_btn.click(fn=create_vector_store, inputs=search_terms, outputs=status)
+    
+    sum_btn.click(fn=generate_summaries, outputs=[sum_status, out])
     gr.Markdown(
         """
         [2025-05-17] initial deployment<br>
@@ -107,6 +131,6 @@ with gr.Blocks() as demo:
         )
     
 if __name__ == "__main__":
-    print('v2025-05-17--1148')
+    print('v2025-05-17--2102')
     print('use `gradio app.py` to run in reload mode')
     demo.launch()
